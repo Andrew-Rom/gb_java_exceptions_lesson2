3d80ff6b6057266e3331bbdaf2ce3a1a92d77605
Andrew-Rom <1811mail@gmail.com>
2023-04-11 21:05:41 +0300

    Task1 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int[] arr = new int[10];
+            System.out.println("Укажите индекс элемента массива, в который хотите записать значение 1");
+
+            if (!scanner.hasNextInt()) {
+                throw new IllegalArgumentException("Incorrect Input");
+            }
+
+            int index = scanner.nextInt();
+
+            if (index < 0 || index >= arr.length) {
+                throw new ArrayIndexOutOfBoundsException("Указан индекс за пределам массива");
+            }
+
+            arr[index] = 1;
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Unknown error");
+        }
     }
 }
